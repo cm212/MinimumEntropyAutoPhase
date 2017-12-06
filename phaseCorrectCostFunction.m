9f896e66e05ae258c2b6776bb90bdd6ceb23ee08
Reed <212669748@logon.ds.ge.com>
2017-12-06 13:45:21 -0600

    bug fixed in negativity penalty

--- phaseCorrectCostFunction.m
+++ phaseCorrectCostFunction.m
@@ -1,17 +1,18 @@
 function entr = phaseCorrectCostFunction(spectrum, params);
   R = real(spectrum);
+  
   h = abs(R) / sum(abs(R));
   entr  = -sum(h .* log(h));
   if(params.nonNegativePenalty)
     % equation 8 in reference
-    stepDownFunction = 0.5 - sign(h)/2; % 1 when negative, 0 when positive 
+    stepDownFunction = 0.5 - sign(R)/2; % 1 when negative, 0 when positive 
   
     % should be determined to make negativity penalty on order of entropy
     % for now hard coded
-    scaleFactor = 1e1;
+    scaleFactor = 1e10;
 
     % equation 7 in reference
-    negativePenalty = scaleFactor * sum(stepDownFunction .* abs(R));
+    negativePenalty = scaleFactor * sum(stepDownFunction .* abs(h));
     entr = entr + negativePenalty;
   end
 end
