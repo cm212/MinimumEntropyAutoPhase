62488bebe313277d78e1d7a3065fb477019f223d
Reed <212669748@logon.ds.ge.com>
2017-12-06 13:04:13 -0600

    readme updates

--- phaseCorrectCostFunction.m
+++ phaseCorrectCostFunction.m
@@ -3,6 +3,7 @@ function entr = phaseCorrectCostFunction(spectrum, params);
   h = abs(h) / sum(abs(h));
   entr  = -sum(h .* log(h));
   if(params.nonNegativePenalty)
-    entr = entr + 0;
+    negativePenalty = 0;
+    entr = entr + negativePenalty;
   end
 end
