f21f3bbe6ff841de425d4939a13c2debd64e100c
Galen Reed <galen.d.reed@gmail.com>
2019-01-07 17:03:35 -0600

    refactoring so that phi1 and phi2 are contained in a single vector

--- phaseCorrect.m
+++ phaseCorrect.m
@@ -1,33 +1,34 @@
 
-function [correctedSpectrum, phi0] = phaseCorrect(spectrum, params)
-  ## NOTE: returns a corrected spectrum and 0 order phase in radians
+function [correctedSpectrum, phiVector] = phaseCorrect(spectrum, params)
+  ## NOTE: returns a corrected spectrum and a 2-vector of the  
+  ##   0th- and 1st-order phases
   
   bruteForce = 0;
   simplexZeroOrder = 1;
   simplexZeroAndFirstOrder = 2;
   correctedSpectrum = [];
-  phi0 = 0;
+  phiVector = [0 0];
+  
   if(params.searchMethod == bruteForce)
     ent = zeros([1 360]);
-    for phi = 1:1:360
-      phiFactor = exp(-1i * pi * phi / 360);
+    for phaseIndex = 1:1:360
+      phiFactor = exp(-1i * pi * phaseIndex / 360);
       spectrumCorr = spectrum .* phiFactor;
-      ent(phi) = phaseCorrectCostFunction(spectrumCorr, params);
+      ent(phaseIndex) = phaseCorrectCostFunction(spectrumCorr, params);
     end  
     minIndex = find(ent == min(ent));
-    phi0 = pi * minIndex / 360;
-    correctedSpectrum = spectrum * exp(-1i * phi0);
+    phiVector(1) = pi * minIndex / 360;
+    correctedSpectrum = spectrum * exp(-1i * phiVector(1));
   elseif(params.searchMethod == simplexZeroOrder)
     init = [0];
-    phi0 = fminsearch (@(phi) (phaseCorrectCostFunction(exp(-1i * phi) * spectrum, params)), init);
-    correctedSpectrum = spectrum * exp(-1i * phi0);
+    phiVector(1) = fminsearch (@(phi) (phaseCorrectCostFunction(exp(-1i * phi) * spectrum, params)), init);
+    correctedSpectrum = spectrum * exp(-1i * phiVector(1));
   elseif(params.searchMethod == simplexZeroAndFirstOrder)
     ramp = linspace(0, 1, length(spectrum));
     init = [0; 0];
     bestPhase = fminsearch (@(phi) (phaseCorrectCostFunction(exp(-1i * (phi(1) + phi(2)*ramp)) .* spectrum' , params)), init);
-    phi0 = bestPhase(1);
-    phi1 = bestPhase(2);% currently not returned by function
-    correctedSpectrum = exp(-1i * (phi0 + phi1*ramp)) .* spectrum';
+    phiVector = bestPhase;
+    correctedSpectrum = exp(-1i * (phiVector(1) + phiVector(2)*ramp)) .* spectrum';
   else
     disp('bad search method');
   end
